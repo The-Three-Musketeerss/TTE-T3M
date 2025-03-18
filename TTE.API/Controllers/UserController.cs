f8b8ca7a08d42c5ad55b9eb5adf369c97d44c7ca
Mateoskix <mateogt0903@gmail.com>
2025-03-17 22:32:46 -0500

    feat: add user controller

---
+++ TTE.API/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TTE.Application.Interfaces;
+using TTE.Commons.Constants;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Authorize]
+    [Route("api/user")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var users = await _userService.GetUsers();
+            return Ok(users);
+        }
+    }
+}
