9e938790fc049239372cf6c1ae02517588a44d35
Mateoskix <mateogt0903@gmail.com>
2025-05-08 13:53:19 -0500

    feat: updated api program.cs to support all origins

--- TTE.API/Program.cs
+++ TTE.API/Program.cs
@@ -8,8 +8,17 @@ builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
 builder.Services.AddSwaggerServices(builder.Configuration);
 
-
 builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
@@ -22,6 +31,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors("AllowAll");
+
 app.UseAuthentication();
 app.UseAuthorization();
 
@@ -30,6 +41,5 @@ app.MapControllers();
 app.MapGet("/", () => Results.Ok(new { status = "Healthy", timestamp = DateTime.UtcNow }))
    .WithName("HealthCheck")
    .WithTags("Health");
-
 
 app.Run();
